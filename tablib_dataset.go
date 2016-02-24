1a7dea61cccddb22c28af1f2c9074c53d3357ed5
Alexandre Grison <a.grison@gmail.com>
2016-02-24 19:53:08 +0100

    Source file refactoring

--- tablib_dataset.go
+++ tablib_dataset.go
@@ -4,9 +4,7 @@
 package tablib
 
 import (
-	"fmt"
 	"sort"
-	"strconv"
 	"time"
 )
 
@@ -497,40 +495,4 @@ func (d *Dataset) Records() [][]string {
 	}
 
 	return records
-}
-
-func isTagged(tag string, tags []string) bool {
-	for _, t := range tags {
-		if t == tag {
-			return true
-		}
-	}
-	return false
-}
-
-func (d *Dataset) asString(vv interface{}) string {
-	var v string
-	switch vv.(type) {
-	case string:
-		v = vv.(string)
-	case int:
-		v = strconv.Itoa(vv.(int))
-	case int64:
-		v = strconv.FormatInt(vv.(int64), 10)
-	case uint64:
-		v = strconv.FormatUint(vv.(uint64), 10)
-	case bool:
-		v = strconv.FormatBool(vv.(bool))
-	case float64:
-		v = strconv.FormatFloat(vv.(float64), 'G', -1, 32)
-	case time.Time:
-		v = vv.(time.Time).Format(time.RFC3339)
-	default:
-		if d.EmptyValue != "" {
-			v = d.EmptyValue
-		} else {
-			v = fmt.Sprintf("%s", v)
-		}
-	}
-	return v
 }
--- tablib_util.go
+++ tablib_util.go
@@ -1,5 +1,11 @@
 package tablib
 
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
 // internalLoadFromDict creates a Dataset from an array of map representing columns.
 func internalLoadFromDict(input []map[string]interface{}) (*Dataset, error) {
 	// retrieve columns
@@ -18,4 +24,42 @@ func internalLoadFromDict(input []map[string]interface{}) (*Dataset, error) {
 	}
 
 	return ds, nil
+}
+
+// isTagged checks if a tag is in an array of tags.
+func isTagged(tag string, tags []string) bool {
+	for _, t := range tags {
+		if t == tag {
+			return true
+		}
+	}
+	return false
+}
+
+// asString returns a value as a string.
+func (d *Dataset) asString(vv interface{}) string {
+	var v string
+	switch vv.(type) {
+	case string:
+		v = vv.(string)
+	case int:
+		v = strconv.Itoa(vv.(int))
+	case int64:
+		v = strconv.FormatInt(vv.(int64), 10)
+	case uint64:
+		v = strconv.FormatUint(vv.(uint64), 10)
+	case bool:
+		v = strconv.FormatBool(vv.(bool))
+	case float64:
+		v = strconv.FormatFloat(vv.(float64), 'G', -1, 32)
+	case time.Time:
+		v = vv.(time.Time).Format(time.RFC3339)
+	default:
+		if d.EmptyValue != "" {
+			v = d.EmptyValue
+		} else {
+			v = fmt.Sprintf("%s", v)
+		}
+	}
+	return v
 }
